fa46f688ca265ba90e43981bf25a9b46e924d21f
jaehuiLee <20201005@sungshin.ac.kr>
2023-07-17 16:18:34 +0900

    init:🎉 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const app = express()
+const dbConfig = require('./models/db')
+const db = dbConfig.db
+require('dotenv').config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+app.set("port", process.env.PORT || 8080)
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+)
 
-export default App;
+app.get("/", (req, res) => {
+    res.send("Welcome")
+})
+
+app.listen(app.get("port"),()=>{
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+    })
+
+app.use('/', require('./routes'));
